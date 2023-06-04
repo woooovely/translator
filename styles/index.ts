e401b7eed2b876dad1be2c3098da61e0d2ae40e6
woosang <woosang3078@hanmail.net>
2023-06-04 14:30:19 +0900

    스타일 파일 추가

---
+++ styles/index.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const TextBoxContainer = styled.div`
+  height: 343px;
+  padding-left: 30px;
+  padding-right: 30px;
+  margin-top: 50px;
+  display: flex;
+`
+
+export const EngTextBox = styled.div`
+  height: 343px;
+  width: 599px;
+  border: 1px solid #CDCDCD;
+  border-radius: 10px;
+  margin-right: 20px;
+`
+
+export const KorTranlatedBox = styled.div`
+  width: 599px;
+  height: 343px;
+  border: 1px solid #CDCDCD;
+  border-radius: 10px;
+`
+
+export const LanguageTitleBox = styled.div`
+  height: 52px;
+  border-bottom: 1px solid #E3E3E3;
+`
+
+export const LanguageTitle = styled.span`
+  font-size: 18px;
+  padding: 40px;
+  padding: 0;
+  display: flex;
+  align-items: center;
+`
